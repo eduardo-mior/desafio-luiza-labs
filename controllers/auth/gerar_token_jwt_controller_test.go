49241e7ce5cf61f4a577539f50b44c0e3aa8e48d
Eduardo Mior <eduardo-mior@hotmail.com>
2022-05-23 13:30:27 -0300

    test: melhora testes unitários do EndPoint de geração de token JWT para testar todos os casos e para utilizar mock

--- controllers/auth/gerar_token_jwt_controller_test.go
+++ controllers/auth/gerar_token_jwt_controller_test.go
@@ -1,21 +1,26 @@
 package auth_controller
 
 import (
-	"desafio_luizalabs/services"
+	"desafio_luizalabs/domain/mocks"
 	"desafio_luizalabs/viewmodels"
 	"encoding/json"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGerarTokenJWT(t *testing.T) {
+func TestGerarTokenJWTSucesso(t *testing.T) {
+
+	serviceAuth := new(mocks.IServiceAuth)
 
 	response := httptest.NewRecorder()
 	c, _ := gin.CreateTestContext(response)
-	GerarTokenJWT(&services.ServiceAuth{})(c)
+
+	serviceAuth.On("GenerateTokenJWT").Return("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiBEb2UifQ.b-bQWDLukCsP-kz255shgiDNoPjycxSizc1cIljoEic", nil)
+	GerarTokenJWT(serviceAuth)(c)
 
 	responseBody := viewmodels.Token{}
 	err := json.NewDecoder(response.Body).Decode(&responseBody)
@@ -23,5 +28,24 @@ func TestGerarTokenJWT(t *testing.T) {
 
 	assert.Equal(t, 200, response.Code)
 	assert.NotEmpty(t, responseBody.Token)
+
+}
+
+func TestGerarTokenJWTErro(t *testing.T) {
+
+	serviceAuth := new(mocks.IServiceAuth)
+
+	response := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(response)
+
+	serviceAuth.On("GenerateTokenJWT").Return("", jwt.ErrInvalidKeyType)
+	GerarTokenJWT(serviceAuth)(c)
+
+	responseBody := viewmodels.Error{}
+	err := json.NewDecoder(response.Body).Decode(&responseBody)
+	assert.Equal(t, nil, err)
+
+	assert.Equal(t, 500, response.Result().StatusCode)
+	assert.Equal(t, viewmodels.Error{Error: responseBody.Error}, responseBody)
 
 }
